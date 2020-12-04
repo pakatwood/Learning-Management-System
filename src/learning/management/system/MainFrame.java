12198f2d37c318cb2d27390c4733aa8da3818260
Haider Qazi <haiderqazi1997@gmail.com>
2020-12-04 00:58:04 -0600

    Learning Management System (2.3)

--- src/learning/management/system/MainFrame.java
+++ src/learning/management/system/MainFrame.java
@@ -30,8 +30,7 @@ import javax.swing.table.TableRowSorter;
 
 /**
  *
- * @author Haider Qazi (Chief Programmer), Daniel Gasperini (Backup Programmer), Javier Blanco (Programmer), David Dinh (Programmer), Christian Francois (Programming Secretary) test test (DAVID WAS HERE)
- * Test
+ * @author Haider Qazi (Chief Programmer), Daniel Gasperini (Backup Programmer), Javier Blanco (Programmer), David Dinh (Programmer), Christian Francois (Programming Secretary)
  * 
  */
 public class MainFrame extends javax.swing.JFrame {
